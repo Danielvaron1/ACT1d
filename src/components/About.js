269702f8f45e4f4d5e2add3a20d9bd4718eb5cef
Danielvaron1 <dani-0153@hotmail.com>
2024-05-17 13:38:42 -0500

    About

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/About.css';
+
+const About = () => {
+    return (
+        <div className="about-section">
+            <h1>Sobre Nosotros</h1>
+            <p>Bienvenidos a Amigurumi Creativo, tu destino para amigurumis únicos hechos a mano con amor. Nos especializamos en crear adorables figuras de crochet, perfectas para regalar o coleccionar. Cada pieza es confeccionada con dedicación y atención al detalle, utilizando materiales de alta calidad.</p>
+            <p>Nuestro equipo de artesanos se enorgullece de ofrecer productos personalizados que capturan la esencia de la artesanía tradicional. Gracias por apoyar nuestro trabajo y permitirnos compartir nuestra pasión contigo.</p>
+        </div>
+    );
+};
+
+export default About;
--- src/routes/GlobalRouter.js
+++ src/routes/GlobalRouter.js
@@ -6,6 +6,7 @@ import {Overview} from "../views/Overview";
 import {Header} from "../components/Header";
 import UserProfile from "../components/UserProfile";
 import ShoppingCart from "../components/ShoppingCart";
+import About from "../components/About";
 
 function GlobalRouter() {
     return (
@@ -15,6 +16,7 @@ function GlobalRouter() {
                 <Route path="/product/:productId" element={<Layout><ProductDetails /></Layout>} />
                 <Route path="/user" element={<Layout><UserProfile /></Layout>} />
                 <Route path="/cart" element={<Layout><ShoppingCart /></Layout>} />
+                <Route path="/about" element={<Layout><About /></Layout>} />
                 <Route path="*" element={<Layout><NotFound /></Layout>} />
             </Routes>
         </BrowserRouter>
